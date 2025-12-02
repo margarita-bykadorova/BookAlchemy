8bf24f4c717a5ed26997c775030a83f73a969c67
Margarita <marharitabykadorova@gmail.com>
2025-12-02 12:28:48 +0100

    Add Author and Book models

--- app.py
+++ app.py
@@ -6,3 +6,11 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data/library.sqlite"
 
 db = SQLAlchemy(app)
 
+class Author(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    author_id = db.Column(db.Integer, db.ForeignKey("author.id"), nullable=False)
