b3aa60a13a1015767c3976add2b986f309197633
Margarita <marharitabykadorova@gmail.com>
2025-12-02 12:35:23 +0100

    Add basic home route

--- app.py
+++ app.py
@@ -14,3 +14,10 @@ class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), nullable=False)
     author_id = db.Column(db.Integer, db.ForeignKey("author.id"), nullable=False)
+
+@app.route("/")
+def home():
+    return "Home Page"
+
+with app.app_context():
+    db.create_all()
